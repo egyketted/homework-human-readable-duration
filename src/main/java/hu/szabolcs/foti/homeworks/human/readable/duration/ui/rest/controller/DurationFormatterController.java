e1c5d26c45125927fd45a15d31ae3367f5a25769
egyketted <szabolcs.foti@gmail.com>
2025-04-23 15:10:41 +0200

    Use spring's ResponseEntity for proper http statuses

--- src/main/java/hu/szabolcs/foti/homeworks/human/readable/duration/ui/rest/controller/DurationFormatterController.java
+++ src/main/java/hu/szabolcs/foti/homeworks/human/readable/duration/ui/rest/controller/DurationFormatterController.java
@@ -3,6 +3,7 @@ package hu.szabolcs.foti.homeworks.human.readable.duration.ui.rest.controller;
 import hu.szabolcs.foti.homeworks.human.readable.duration.service.duration.DurationFormatterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,20 +16,20 @@ public class DurationFormatterController {
     private final DurationFormatterService service;
 
     @GetMapping("/api/{duration}")
-    public String getHumanReadableFormat(@PathVariable String duration) {
+    public ResponseEntity<String> getHumanReadableFormat(@PathVariable String duration) {
         //accept malformed input for proper error handling
         int seconds;
         if (!duration.matches("\\d+")) {
-            return String.format("Duration should be an integer. '%s' is not an integer.", duration);
+            return ResponseEntity.status(418).body(String.format("Duration should be an integer. '%s' is not an integer.", duration));
         } else {
             //overflow check
             try {
                 seconds = Integer.parseInt(duration);
             } catch (NumberFormatException e) {
-                return String.format("'%s' is too large for an integer", duration);
+                return ResponseEntity.badRequest().body(String.format("'%s' is too large for an integer", duration));
             }
         }
 
-        return service.formatDuration(seconds);
+        return ResponseEntity.ok(service.formatDuration(seconds));
     }
 }
