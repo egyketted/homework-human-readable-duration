db6e1b74370f5da68e3b84f75435d84dd291a51b
egyketted <szabolcs.foti@gmail.com>
2025-04-23 09:18:05 +0200

    Add basic spring boot application setup

---
+++ src/main/java/hu/szabolcs/foti/homeworks/human/readable/duration/HumanReadableDurationApplication.java
@@ -0,0 +1,12 @@
+package hu.szabolcs.foti.homeworks.human.readable.duration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanReadableDurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumanReadableDurationApplication.class, args);
+    }
+} 
---
+++ src/test/java/hu/szabolcs/foti/homeworks/human/readable/duration/HumanReadableDurationApplicationTest.java
@@ -0,0 +1,12 @@
+package hu.szabolcs.foti.homeworks.human.readable.duration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanReadableDurationApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+} 
